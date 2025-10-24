5735176fb21804c9ed87714b8f6cfcf77514f5eb
Pavan Thanuj Upadrasta <49611451+n00bcodr@users.noreply.github.com>
2025-10-24 14:16:33 +0530

    Update api-docs slash to include user api-docs url

--- commands/api-docs.js
+++ commands/api-docs.js
@@ -5,6 +5,6 @@ export default {
 		.setName('api-docs')
 		.setDescription('Official Jellyfin documentation'),
 	async execute(interaction) {
-		await interaction.reply("📖🔌 Official Jellyfin API Documentation: https://api.jellyfin.org/");
+		await interaction.reply("📖🔌 Official Jellyfin API Documentation: https://api.jellyfin.org/\n\n📖👤 Your Installation's API Documentation: [YOUR_JELLYFIN_URL]/api-docs/swagger/index.html");
 	},
 };
