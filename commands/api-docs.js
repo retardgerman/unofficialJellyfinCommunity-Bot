7bd80d0f95f631009e865e42704daca991913be5
Pavan Thanuj Upadrasta <49611451+n00bcodr@users.noreply.github.com>
2025-10-24 12:19:56 +0530

    Fix api-docs slash command

--- commands/api-docs.js
+++ commands/api-docs.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('docs')
+		.setName('api-docs')
 		.setDescription('Official Jellyfin documentation'),
 	async execute(interaction) {
 		await interaction.reply("📖🔌 Official Jellyfin API Documentation: https://api.jellyfin.org/");
